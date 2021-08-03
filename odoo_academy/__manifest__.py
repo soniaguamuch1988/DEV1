ace8f8e8452d34b43f0b148fa90994d31add71cb
soniaguamuch1988 <sonia.guamuch@gmail.com>
2021-08-03 00:28:42 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    
+    'summary': """Academy app to manage training""",
+    
+    'Descripciotn': """
+        Academy MOdule to manage Trainig:
+        -Courses
+        -Sessiones
+        -Attendees
+    """,
+    
+    'author': 'Sonia Guamuch',
+    
+    'website':'https://www.odoo.com',
+    
+     'category': 'Training',
+    
+    'version': '0.1',
+    'depends': ['base'],
+    
+    'data': [    
+        
+    ],
+    
+    'demo':[
+        
+        
+    ],
+    
+    
+}
